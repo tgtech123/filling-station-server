b6d21942b0a92669a7f194412504c7b36c9fb197
nnamdi <uzonnamdi31@gmail.com>
2025-11-06 16:16:42 +0100

    new

--- src/routes/tank.route.ts
+++ src/routes/tank.route.ts
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post("/add-tank", requireAuth, checkRole("manager"), addTank);
 router.get("/", requireAuth, checkRole("manager"), getTankPerStation);
 router.post("/update-tank", requireAuth, checkRole("manager"), updateTankDetails);
-router.post("/delete-tank", requireAuth, checkRole("manager"), deleteTank);
+router.post("/delete-tank/:tankId", requireAuth, checkRole("manager"), deleteTank);
 
 
 
